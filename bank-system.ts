a1c2cd3c2276c27516e0646668ad184e619688f5
Phemelo Shopelekae <shopelekae.phemelo11@gmail.com>
2026-09-17 00:19:13 +0200

    added type to failure() of type string

--- bank-system.ts
+++ bank-system.ts
@@ -1,3 +1,8 @@
 const WITHDRAWAL_WINDOW_MS: number = 10 * 1000; // 10 seconds
 const MAX_WITHDRAWALS_IN_WINDOW: number = 3;
 const SUSPICIOUS_MULTIPLIER: number = 5;
+
+
+function failure(message: string) {
+  return { ok: false, error: message };
+}
